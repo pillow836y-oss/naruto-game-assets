13a311020b9eff971756ac8235b9cfe73b6afea3
pillow836y-oss <pillow836y@gmail.com>
2026-03-15 15:06:51 -0700

    Add character data for Naruto series

--- characters.js
+++ characters.js
@@ -1,1 +1,62 @@
-
+const characters = [
+{
+name: "Naruto Uzumaki",
+image: "https://static.wikia.nocookie.net/naruto/images/7/7d/Naruto_Part_II.png"
+},
+{
+name: "Sasuke Uchiha",
+image: "https://static.wikia.nocookie.net/naruto/images/5/5f/Sasuke_Part_II.png"
+},
+{
+name: "Sakura Haruno",
+image: "https://static.wikia.nocookie.net/naruto/images/7/76/Sakura_Part_II.png"
+},
+{
+name: "Kakashi Hatake",
+image: "https://static.wikia.nocookie.net/naruto/images/e/e1/Kakashi_Part_II.png"
+},
+{
+name: "Itachi Uchiha",
+image: "https://static.wikia.nocookie.net/naruto/images/b/bb/Itachi.png"
+},
+{
+name: "Madara Uchiha",
+image: "https://static.wikia.nocookie.net/naruto/images/3/32/Madara_Uchiha.png"
+},
+{
+name: "Obito Uchiha",
+image: "https://static.wikia.nocookie.net/naruto/images/0/08/Obito_Uchiha.png"
+},
+{
+name: "Minato Namikaze",
+image: "https://static.wikia.nocookie.net/naruto/images/7/7c/Minato_Namikaze.png"
+},
+{
+name: "Tsunade",
+image: "https://static.wikia.nocookie.net/naruto/images/9/9f/Tsunade.png"
+},
+{
+name: "Jiraiya",
+image: "https://static.wikia.nocookie.net/naruto/images/7/7c/Jiraiya.png"
+},
+{
+name: "Gaara",
+image: "https://static.wikia.nocookie.net/naruto/images/3/34/Gaara.png"
+},
+{
+name: "Rock Lee",
+image: "https://static.wikia.nocookie.net/naruto/images/3/34/Rock_Lee.png"
+},
+{
+name: "Hinata Hyuga",
+image: "https://static.wikia.nocookie.net/naruto/images/9/97/Hinata_Part_II.png"
+},
+{
+name: "Shikamaru Nara",
+image: "https://static.wikia.nocookie.net/naruto/images/9/9b/Shikamaru_Part_II.png"
+},
+{
+name: "Neji Hyuga",
+image: "https://static.wikia.nocookie.net/naruto/images/6/6c/Neji_Hyuga.png"
+}
+];
